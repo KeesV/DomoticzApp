f84a18a7cc9672f3259c901cea85451977e8558e
Kees Verhaar <keesv@delta-n.nl>
2016-01-03 18:10:32 +0100

    Initial very simple working version

---
+++ AutoHome/AutoHome/MainPage.xaml.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AutoHome
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        ObservableCollection<LightSwitch> observableSwitches = new ObservableCollection<LightSwitch>();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            SwitchesCVS.Source = observableSwitches;
+        }
+
+        private void PowerOff_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private async Task addToLog()
+        {
+            string command = string.Format("http://192.168.1.4:8080/json.htm?type=command&param=addlogmessage&message=HelloFromApp");
+
+            WebRequest wReq = WebRequest.Create(command);
+
+            WebResponse response = await wReq.GetResponseAsync();
+            
+        } 
+
+        private void btnRadio_Click(object sender, RoutedEventArgs e)
+        {
+            addToLog();
+
+        }
+
+        private async Task<AvailableSwitchesResponse> getAvailableSwitchesAsync()
+        {
+            string UrlRequest = "http://192.168.1.4:8080/json.htm?type=command&param=getlightswitches";
+
+            try
+            {
+                HttpWebRequest request = WebRequest.Create(UrlRequest) as HttpWebRequest;
+
+                using (HttpWebResponse response = (await request.GetResponseAsync()) as HttpWebResponse)
+                {
+                    if(response.StatusCode != HttpStatusCode.OK)
+                    {
+                        throw new Exception(String.Format("Server error (HTTP {0}: {1}).",
+                            response.StatusCode,
+                            response.StatusDescription));
+                    }
+                    DataContractJsonSerializer jsonSerializer = new DataContractJsonSerializer(typeof(AvailableSwitchesResponse));
+                    object objResponse = jsonSerializer.ReadObject(response.GetResponseStream());
+                    AvailableSwitchesResponse jsonResponse = objResponse as AvailableSwitchesResponse;
+                    return jsonResponse;
+                }
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+
+        }
+
+        private async void Page_Loading(FrameworkElement sender, object args)
+        {
+
+            //ContactsCVS.Source = Contact.GetContactsGrouped(250);
+            AvailableSwitchesResponse availableSwitches = await getAvailableSwitchesAsync();
+            
+            foreach (LightSwitch s in availableSwitches.LightSwitches)
+            {
+                observableSwitches.Add(s);
+            }
+        }
+
+        private async Task setSwitchAsync(LightSwitch switchToSwitch, Boolean value)
+        {
+            string switchcmd = value == true ? "On" : "False";
+            string UrlRequest = string.Format("http://192.168.1.4:8080/json.htm?type=command&param=switchlight&idx={0}&switchcmd={1}",switchToSwitch.idx, switchcmd);
+
+            try
+            {
+                HttpWebRequest request = WebRequest.Create(UrlRequest) as HttpWebRequest;
+
+                using (HttpWebResponse response = (await request.GetResponseAsync()) as HttpWebResponse)
+                {
+                    if (response.StatusCode != HttpStatusCode.OK)
+                    {
+                        throw new Exception(String.Format("Server error (HTTP {0}: {1}).",
+                            response.StatusCode,
+                            response.StatusDescription));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        private async void btnSwitch_Click(object sender, RoutedEventArgs e)
+        {
+            if(lstSwitches.SelectedItems.Count > 0)
+            {
+                LightSwitch selectedSwitch = lstSwitches.SelectedItem as LightSwitch;
+                await setSwitchAsync(selectedSwitch, true);
+
+            }
+
+        }
+    }
+
+    [DataContract]
+    public class AvailableSwitchesResponse
+    {
+        [DataMember(Name = "result")]
+        public LightSwitch[] LightSwitches { get; set; }
+
+        [DataMember(Name = "status")]
+        public string Status { get; set; }
+
+        [DataMember(Name = "title")]
+        public string Title { get; set; }
+    }
+
+    [DataContract]
+    public class LightSwitch
+    {
+        [DataMember(Name = "IsDimmer")]
+        public Boolean IsDimmer { get; set; }
+
+        [DataMember(Name = "Name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "SubType")]
+        public string SubType { get; set; }
+
+        [DataMember(Name ="Type")]
+        public string Type { get; set; }
+
+        [DataMember(Name = "idx")]
+        public int idx { get; set; }
+    }
+}
