c6b64c633c064e44a82f1010f96b1d75ac1d1745
Kees Verhaar <keesv@delta-n.nl>
2016-01-04 17:02:27 +0100

    Added project based on Template10

---
+++ AutoHome/AutoHome.Universal/Views/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+using AutoHome.Universal.ViewModels;
+using Windows.UI.Xaml.Controls;
+
+namespace AutoHome.Universal.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Disabled;
+        }
+
+        // strongly-typed view models enable x:bind
+        public MainPageViewModel ViewModel => this.DataContext as MainPageViewModel;
+    }
+}
